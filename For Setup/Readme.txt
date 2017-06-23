adb06b1f9e43505c45252ac0d3c5e896c4a57f9b
Shynet <shynet@gmail.com>
2017-06-23 23:24:53 +0300

    Changed default hide from tray to false and added setup

---
+++ For Setup/Readme.txt
@@ -0,0 +1,5 @@
+MDReplacer is a simple utility aimed for Windows 10 (and above+). It allows moving between desktops easily using the Shift + Mouse wheel up\down.
+Make sure you have multiple desktops before trying to move between them. You can create a new desktop by clicking on Ctrl + Win + D.
+
+-------------------------------------------
+By Shynet © Nemex 2017 , visit us at : http://byshynet.com .
--- MDReplacer/Properties/Settings.Designer.cs
+++ MDReplacer/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace MDReplacer.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool HideFromTray {
             get {
                 return ((bool)(this["HideFromTray"]));
